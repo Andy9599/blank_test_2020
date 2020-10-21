0dad06afe197d6e2c8e09b9bbee0ef57d29b1085
Samuel Bancal <Samuel.Bancal@epfl.ch>
2020-10-21 10:51:06 +0200

    Added logic to extract_temperatures function
    Tested :
    
    $ ./process_aigle_temperature.py
    Year 1981 has 365 values
    January 1981 has 31 values
    February 1981 has 28 values
    Year 1984 has 366 values
    February 1984 has 29 values
    April 2001 has 30 values
    [[ 3.1]
     [ 4.3]
     [ 5.8]
     [ 8.4]
     [ 5.5]
     [ 6.7]
     [ 9. ]
     [ 5.8]
     [ 6.9]
     [ 7.5]
     [ 6.2]
     [ 4.9]
     [ 5.6]
     [ 3.3]
     [ 4.8]
     [ 4.3]
     [ 1.3]
     [ 2.9]
     [ 5.3]
     [ 3.7]
     [ 1.8]
     [ 3.9]
     [ 4.6]
     [ 5.8]
     [ 9.2]
     [ 6.6]
     [ 7.5]
     [10.6]
     [10.6]
     [ 8.6]]
    
    git add .
    git commit

--- process_aigle_temperature.py
+++ process_aigle_temperature.py
@@ -13,6 +13,7 @@ import numpy as np
 import pandas as pd
 
 SOURCE_FILE = 'aigle_1980-12-31_2019-07-09.csv'
+NB_DAYS_PER_MONTH = (0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 
 temperatures_aigle = pd.read_csv(
     SOURCE_FILE,
@@ -30,8 +31,36 @@ def extract_temperatures(year, month=0):
     B) extract_temperatures(year, month)
        -> from year-month-01 - year-month-last_day
     '''
-    pass
+    if month == 0:
+        # all year
+        start_date = '%s-01-01' % year
+        end_date = '%s-12-31' % year
+    else:
+        # only one month
+        start_date = '%s-%s-01' % (year, month)
+        end_date = '%s-%s-%s' % (year, month, NB_DAYS_PER_MONTH[month])
+        if month == 2 and year % 4 == 0:
+            # it is leap year!
+            end_date = '%s-%s-29' % (year, month)
+    return temperatures_aigle[start_date:end_date].values
 
 
 if __name__ == '__main__':
-    print(help(extract_temperatures))
+    year_1981 = extract_temperatures(1981)
+    print('Year 1981 has %s values' % len(year_1981))
+
+    jan_1981 = extract_temperatures(1981, 1)
+    print('January 1981 has %s values' % len(jan_1981))
+
+    feb_1981 = extract_temperatures(1981, 2)
+    print('February 1981 has %s values' % len(feb_1981))
+
+    year_1984 = extract_temperatures(1984)
+    print('Year 1984 has %s values' % len(year_1984))
+
+    feb_1984 = extract_temperatures(1984, 2)
+    print('February 1984 has %s values' % len(feb_1984))
+
+    apr_2001 = extract_temperatures(2001, 4)
+    print('April 2001 has %s values' % len(apr_2001))
+    print(apr_2001)

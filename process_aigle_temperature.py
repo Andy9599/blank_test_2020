b655886f5f24aa2c5b0a9ace39ca9a02e258d437
Samuel Bancal <Samuel.Bancal@epfl.ch>
2020-10-21 10:15:46 +0200

    Add process_aigle_temperature.py with a description of what it will do
    git add process_aigle_temperature.py
    git commit

---
+++ process_aigle_temperature.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+'''
+This Python script will
+
++ Read and parse daily temperature from Aigle weather station
++ Extract monthly and yearly means for the following years :
+  1981, 1991, 2001, 2011
++ Save a graph of those in the file `aigle_temperature_means.png`.
+'''

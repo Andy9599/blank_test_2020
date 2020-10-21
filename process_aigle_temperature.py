76b4b88b55649864bbfdcbe35e36b881f28e87dc
Samuel Bancal <Samuel.Bancal@epfl.ch>
2020-10-21 10:21:56 +0200

    Read and parsie CSV file with pandas
    git add .
    git commit

--- process_aigle_temperature.py
+++ process_aigle_temperature.py
@@ -8,3 +8,24 @@ This Python script will
   1981, 1991, 2001, 2011
 + Save a graph of those in the file `aigle_temperature_means.png`.
 '''
+
+import numpy as np
+import pandas as pd
+
+SOURCE_FILE = 'aigle_1980-12-31_2019-07-09.csv'
+
+temperatures_aigle = pd.read_csv(
+    SOURCE_FILE,
+    sep=';', usecols=[1, 2], index_col=0,
+    parse_dates=True, dtype={'t06200ds': np.float64},
+    na_values='-'
+)
+
+
+if __name__ == '__main__':
+
+    start_date = '1981-01-01'
+    end_date = '1981-12-31'
+    temperatures = temperatures_aigle[start_date:end_date].values
+
+    print(temperatures)
